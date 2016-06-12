3e1f3ff689b1f9f09d50ca4716dc8ba77da6c51e
tranhoangduong1994 <tranhoangduong1994@gmail.com>
2016-06-12 10:44:29 +0000

    fixed a minor stupid bug

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
-    sqlite = new Sequelize(process.env.DATABASE_URL, {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres'
     });
 } else {
