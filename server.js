cdf81045b3446e35c43b5daa953a9dcdc0edc710
tranhoangduong1994 <tranhoangduong1994@gmail.com>
2016-06-12 04:34:03 +0000

    add to model and POST /todos

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var _ = require("underscore");
-
+var db = require("./db.js");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -54,20 +54,31 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
     var body =  _.pick(req.body, 'description', 'completed');
     
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || 
-    body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
     
-    body.description = body.description.trim();
-    // set body.description to be trimmed value
+    db.todo.create(body).then(function(todo) {
+        res.json(todo.toJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+    // call create on db.todo
+    //  respond with 200 and todo
+    //  e res.status(400).json(e)
     
-    body.id = todoNextId;
-    todoNextId++;
+    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || 
+    // body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
     
-    todos.push(body);
+    // body.description = body.description.trim();
+    // // set body.description to be trimmed value
     
-    res.json(body);
+    // body.id = todoNextId;
+    // todoNextId++;
+    
+    // todos.push(body);
+    
+    // res.json(body);
+    
 });
 
 // DELETE /todos/:id
@@ -111,6 +122,8 @@ app.put('/todos/:id', function(req, res) {
     res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-   console.log('Express listening at port ' + PORT + '!') ;
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log('Express listening on port ' + PORT + '!');
+    })
 });
